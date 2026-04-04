7655b0faf421790906d4781b18f09d7a985bee02
cheezy123321 <usagithub20@gmail.com>
2026-04-04 11:08:47 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
 const DATABASE = "mongodb+srv://SithumKalhara:97531@cluster0.iva7dbo.mongodb.net/?retryWrites=true&w=majority" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "94782372020" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
+const BOT_NUMBER = "94724304850" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~XOiWYT1nhw3taMW" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
